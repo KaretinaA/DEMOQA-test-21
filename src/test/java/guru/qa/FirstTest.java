96f66d99c2e542196985c7e8cac75a8586429524
KaretinaA <aleksanna-92@mail.ru>
2023-07-19 23:46:23 +0300

    added FirstTest.java

--- src/test/java/guru/qa/FirstTest.java
+++ src/test/java/guru/qa/FirstTest.java
@@ -5,24 +5,27 @@ import org.junit.jupiter.api.*;
 
 public class FirstTest {
     @BeforeAll
-    static void  BeforeAll() {
+    static void BeforeAll() {
         System.out.println("        BeforeAll");
         Configuration.browser = "chrome";
-            }
+    }
+
     @AfterAll
-    static void  AfterAll() {
+    static void AfterAll() {
         System.out.println("        AfterAll");
     }
+
     @BeforeEach
-  void  BeforeEach() {
+    void BeforeEach() {
         //   open("https://google.com");
         System.out.println("    BeforeEach");
     }
+
     @AfterEach
-    void  AfterEach() {
-        //   open("https://google.com");
+    void AfterEach() {
         System.out.println("    AfterEach");
     }
+
     @Test
     void firsTest() {
 
@@ -30,11 +33,13 @@ public class FirstTest {
         Assertions.assertTrue(3 > 2);
 
     }
+
     @Test
     void secondTest() {
         System.out.println("        secondTest");
         Assertions.assertTrue(3 > 2);
     }
+
     @Test
     void thirdTest() {
         System.out.println("        thirdTest");
