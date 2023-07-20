db2d415679c0955f9c331d0adcd6f97ae0cbe82c
KaretinaA <aleksanna-92@mail.ru>
2023-07-20 17:39:48 +0300

    added TextBoxTest.java

---
+++ src/test/java/comdemoqa/TextBoxTest.java
@@ -0,0 +1,35 @@
+package comdemoqa;
+
+import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+
+public class TextBoxTest {
+    @BeforeAll
+    static void BeforeAll() {
+        Configuration.baseUrl = "https://demoqa.com/";
+        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";
+    }
+    @Test
+    void fillFormTest() {
+        open("/text-box");
+        $("#userName").setValue("Anna");
+        $("#userEmail").setValue("Anna@mail.ru");
+        $("#currentAddress").setValue("Moscow");
+        $("#permanentAddress").setValue("Syberia");
+        $("#submit").click();
+       $("#output").$("#name").shouldHave(text("Anna"));
+
+
+
+
+
+    }
+}
